54e73b89cac0b7e0e8423f58ad656bbb72ec37a5
Jakob Wood <>
2024-11-20 15:57:26 -0500

    Formatting

--- js/upt-script.js
+++ js/upt-script.js
@@ -1,148 +1,151 @@
-jQuery(document).ready(function($) {
-    var urlParams = new URLSearchParams(window.location.search);
-    var paramsToTrack = upt_params.trackedParams;
-    var trackAll = upt_params.trackAll;
-    var cookieLifetime = upt_params.cookieLifetime;
-    var paramFieldMapping = upt_params.paramFieldMapping;
-    var mappings = {};
+jQuery(document).ready(function ($) {
+  var urlParams = new URLSearchParams(window.location.search);
+  var paramsToTrack = upt_params.trackedParams;
+  var trackAll = upt_params.trackAll;
+  var cookieLifetime = upt_params.cookieLifetime;
+  var paramFieldMapping = upt_params.paramFieldMapping;
+  var mappings = {};
 
-    // console.log("URL Parameters:", window.location.search);
-    // console.log("Parameters to Track:", paramsToTrack);
-    // console.log("Track All:", trackAll);
-    // console.log("Cookie Lifetime:", cookieLifetime);
-    // console.log("Parameter to Field Mapping Raw:", paramFieldMapping);
+  // console.log("URL Parameters:", window.location.search);
+  // console.log("Parameters to Track:", paramsToTrack);
+  // console.log("Track All:", trackAll);
+  // console.log("Cookie Lifetime:", cookieLifetime);
+  // console.log("Parameter to Field Mapping Raw:", paramFieldMapping);
 
-    // Parse parameter to field mappings
-    if (paramFieldMapping) {
-        paramFieldMapping.split('\n').forEach(function(line, index) {
-            line = line.trim();
-            if (line === '') return; // Skip empty lines
+  // Parse parameter to field mappings
+  if (paramFieldMapping) {
+    paramFieldMapping.split("\n").forEach(function (line, index) {
+      line = line.trim();
+      if (line === "") return; // Skip empty lines
 
-            // Split only at the first '='
-            var indexOfEqual = line.indexOf('=');
-            if (indexOfEqual === -1) {
-                // console.warn("Invalid mapping line (expected format 'parameter=selector'):", line);
-                return;
-            }
+      // Split only at the first '='
+      var indexOfEqual = line.indexOf("=");
+      if (indexOfEqual === -1) {
+        // console.warn("Invalid mapping line (expected format 'parameter=selector'):", line);
+        return;
+      }
 
-            var paramsPart = line.substring(0, indexOfEqual).trim();
-            var fieldSelector = line.substring(indexOfEqual + 1).trim();
+      var paramsPart = line.substring(0, indexOfEqual).trim();
+      var fieldSelector = line.substring(indexOfEqual + 1).trim();
 
-            if (!paramsPart || !fieldSelector) {
-                // console.warn("Invalid mapping line (empty parameter or selector):", line);
-                return;
-            }
+      if (!paramsPart || !fieldSelector) {
+        // console.warn("Invalid mapping line (empty parameter or selector):", line);
+        return;
+      }
 
-            // Split multiple parameters separated by commas
-            var params = paramsPart.split(',').map(function(p) { return p.trim(); });
+      // Split multiple parameters separated by commas
+      var params = paramsPart.split(",").map(function (p) {
+        return p.trim();
+      });
 
-            // Initialize the array if the selector already exists
-            if (!mappings[fieldSelector]) {
-                mappings[fieldSelector] = [];
-            }
+      // Initialize the array if the selector already exists
+      if (!mappings[fieldSelector]) {
+        mappings[fieldSelector] = [];
+      }
 
-            mappings[fieldSelector] = mappings[fieldSelector].concat(params);
+      mappings[fieldSelector] = mappings[fieldSelector].concat(params);
 
-            // console.log("Mapping Line " + (index + 1) + ":");
-            // console.log("  Parameters:", params);
-            // console.log("  Field Selector:", fieldSelector);
-        });
+      // console.log("Mapping Line " + (index + 1) + ":");
+      // console.log("  Parameters:", params);
+      // console.log("  Field Selector:", fieldSelector);
+    });
+  } else {
+    // console.warn("No parameter to field mappings found.");
+  }
+
+  // Function to set cookie
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie =
+      name + "=" + encodeURIComponent(value) + expires + "; path=/";
+    // console.log("Set Cookie:", name, "=", value);
+  }
+
+  // Function to get cookie
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i].trim();
+      if (c.indexOf(nameEQ) === 0) {
+        var value = decodeURIComponent(c.substring(nameEQ.length, c.length));
+        // console.log("Get Cookie:", name, "=", value);
+        return value;
+      }
+    }
+    // console.log("Get Cookie:", name, "not found.");
+    return null;
+  }
+
+  // Store parameters as cookies
+  var storedParams = {};
+
+  // Store URL parameters
+  urlParams.forEach(function (value, key) {
+    if (trackAll || paramsToTrack.includes(key)) {
+      setCookie("upt_" + key, value, cookieLifetime);
+      storedParams[key] = value;
+    }
+  });
+
+  // Store current URL and referrer
+  setCookie("upt_current_url", window.location.href, cookieLifetime);
+  storedParams["current_url"] = window.location.href;
+
+  var referrer = document.referrer || "Direct";
+  setCookie("upt_referrer", referrer, cookieLifetime);
+  storedParams["referrer"] = referrer;
+
+  // Autofill form fields from cookies
+  Object.keys(mappings).forEach(function (fieldSelector) {
+    var params = mappings[fieldSelector];
+    var values = [];
+
+    params.forEach(function (param) {
+      var value = getCookie("upt_" + param);
+      if (value) {
+        values.push(value);
+      }
+    });
+
+    if (values.length > 0) {
+      var $field = $(fieldSelector);
+      if ($field.length) {
+        var currentVal = $field.val();
+        var newVal = values.join(", ");
+
+        // If the field already has a value, append to it
+        if (currentVal) {
+          newVal = currentVal + ", " + newVal;
+        }
+
+        // console.log("Autofilling Field:", fieldSelector, "with value:", newVal);
+        $field.val(newVal);
+      } else {
+        // console.warn("Field not found for selector:", fieldSelector);
+      }
     } else {
-        // console.warn("No parameter to field mappings found.");
+      // console.log("No values to autofill for selector:", fieldSelector);
     }
+  });
 
-    // Function to set cookie
-    function setCookie(name, value, days) {
-        var expires = '';
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/";
-        // console.log("Set Cookie:", name, "=", value);
-    }
-
-    // Function to get cookie
-    function getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i = 0; i < ca.length; i++) {
-            var c = ca[i].trim();
-            if (c.indexOf(nameEQ) === 0) {
-                var value = decodeURIComponent(c.substring(nameEQ.length, c.length));
-                // console.log("Get Cookie:", name, "=", value);
-                return value;
-            }
-        }
-        // console.log("Get Cookie:", name, "not found.");
-        return null;
-    }
-
-    // Store parameters as cookies
-    var storedParams = {};
-
-    // Store URL parameters
-    urlParams.forEach(function(value, key) {
-        if (trackAll || paramsToTrack.includes(key)) {
-            setCookie('upt_' + key, value, cookieLifetime);
-            storedParams[key] = value;
-        }
-    });
-
-    // Store current URL and referrer
-    setCookie('upt_current_url', window.location.href, cookieLifetime);
-    storedParams['current_url'] = window.location.href;
-
-    var referrer = document.referrer || 'Direct';
-    setCookie('upt_referrer', referrer, cookieLifetime);
-    storedParams['referrer'] = referrer;
-
-    // Autofill form fields from cookies
-    Object.keys(mappings).forEach(function(fieldSelector) {
-        var params = mappings[fieldSelector];
-        var values = [];
-
-        params.forEach(function(param) {
-            var value = getCookie('upt_' + param);
-            if (value) {
-                values.push(value);
-            }
-        });
-
-        if (values.length > 0) {
-            var $field = $(fieldSelector);
-            if ($field.length) {
-                var currentVal = $field.val();
-                var newVal = values.join(', ');
-
-                // If the field already has a value, append to it
-                if (currentVal) {
-                    newVal = currentVal + ', ' + newVal;
-                }
-
-                // console.log("Autofilling Field:", fieldSelector, "with value:", newVal);
-                $field.val(newVal);
-            } else {
-                // console.warn("Field not found for selector:", fieldSelector);
-            }
-        } else {
-            // console.log("No values to autofill for selector:", fieldSelector);
-        }
-    });
-
-    // Send session data to server if any parameters were stored
-    if (Object.keys(storedParams).length > 0) {
-        // console.log("Sending session data to server:", storedParams);
-        $.post(upt_params.ajaxUrl, {
-            action: 'upt_store_session_data',
-            sessionData: storedParams
-        })
-        .done(function(response) {
-            // console.log("Session data stored successfully:", response);
-        })
-        .fail(function(error) {
-            // console.error("Error storing session data:", error);
-        });
-    }
+  // Send session data to server if any parameters were stored
+  if (Object.keys(storedParams).length > 0) {
+    // console.log("Sending session data to server:", storedParams);
+    $.post(upt_params.ajaxUrl, {
+      action: "upt_store_session_data",
+      sessionData: storedParams,
+    })
+      .done(function (response) {
+        // console.log("Session data stored successfully:", response);
+      })
+      .fail(function (error) {
+        // console.error("Error storing session data:", error);
+      });
+  }
 });
--- url-parameter-tracker.php
+++ url-parameter-tracker.php
@@ -7,7 +7,7 @@ Author: LFMC
 */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit;
 }
 
@@ -26,7 +26,8 @@ $myUpdateChecker->setBranch('main');
 
 // Enqueue the JavaScript file
 add_action('wp_enqueue_scripts', 'upt_enqueue_scripts');
-function upt_enqueue_scripts() {
+function upt_enqueue_scripts()
+{
     wp_enqueue_script('upt-script', plugin_dir_url(__FILE__) . 'js/upt-script.js', array('jquery'), '1.4', true);
 
     // Get plugin settings
@@ -43,7 +44,8 @@ function upt_enqueue_scripts() {
 
 // Add top-level menu and submenu pages
 add_action('admin_menu', 'upt_add_admin_menus');
-function upt_add_admin_menus() {
+function upt_add_admin_menus()
+{
     // Add top-level menu
     add_menu_page(
         'URL Parameter Tracker',         // Page title
@@ -78,7 +80,8 @@ function upt_add_admin_menus() {
 
 // Register settings
 add_action('admin_init', 'upt_settings_init');
-function upt_settings_init() {
+function upt_settings_init()
+{
     register_setting('upt_settings_group', 'upt_settings');
 
     add_settings_section(
@@ -138,46 +141,52 @@ function upt_settings_init() {
 }
 
 // Settings section callbacks
-function upt_settings_section_callback() {
+function upt_settings_section_callback()
+{
     echo '<p>Configure how URL parameters are tracked and autofilled into forms.</p>';
 }
 
-function upt_session_management_section_callback() {
+function upt_session_management_section_callback()
+{
     echo '<p>Manage the session data stored by the plugin to prevent excessive database growth.</p>';
 }
 
 // Render functions for settings fields
-function upt_tracked_params_render() {
+function upt_tracked_params_render()
+{
     $options = get_option('upt_settings');
     $tracked_params = isset($options['tracked_params']) ? $options['tracked_params'] : '';
-    ?>
+?>
     <input type='text' name='upt_settings[tracked_params]' value='<?php echo esc_attr($tracked_params); ?>' style="width: 100%;">
     <p class="description">Enter a comma-separated list of URL parameters you want to track (e.g., utm_source, utm_medium). These parameters will be stored and autofilled into forms based on your mappings.</p>
-    <?php
+<?php
 }
 
-function upt_track_all_render() {
+function upt_track_all_render()
+{
     $options = get_option('upt_settings');
     $track_all = isset($options['track_all']) ? $options['track_all'] : 0;
-    ?>
+?>
     <input type='checkbox' name='upt_settings[track_all]' <?php checked($track_all, 1); ?> value='1'>
     <p class="description">If checked, all URL parameters will be tracked and stored, regardless of the list above.</p>
-    <?php
+<?php
 }
 
-function upt_cookie_lifetime_render() {
+function upt_cookie_lifetime_render()
+{
     $options = get_option('upt_settings');
     $cookie_lifetime = isset($options['cookie_lifetime']) ? intval($options['cookie_lifetime']) : 30;
-    ?>
+?>
     <input type='number' name='upt_settings[cookie_lifetime]' value='<?php echo esc_attr($cookie_lifetime); ?>' min="1">
     <p class="description">Specify how many days the tracked parameters should be stored in the user's browser cookies. This ensures parameters persist across multiple pages and sessions.</p>
-    <?php
+<?php
 }
 
-function upt_param_field_mapping_render() {
+function upt_param_field_mapping_render()
+{
     $options = get_option('upt_settings');
     $param_field_mapping = isset($options['param_field_mapping']) ? $options['param_field_mapping'] : '';
-    ?>
+?>
     <textarea name='upt_settings[param_field_mapping]' rows='10' cols='50' style="width:100%;"><?php echo esc_textarea($param_field_mapping); ?></textarea>
     <p class="description">
         Define how tracked parameters are mapped to form fields. Use one mapping per line in the format:<br><br>
@@ -191,13 +200,14 @@ function upt_param_field_mapping_render() {
         In the first example, the values of <code>utm_source</code>, <code>utm_medium</code>, and <code>utm_campaign</code> are inserted into the field with ID <code>all-utms</code>.<br>
         In the second example, labels are added before the values.
     </p>
-    <?php
+<?php
 }
 
-function upt_session_cleanup_interval_render() {
+function upt_session_cleanup_interval_render()
+{
     $options = get_option('upt_settings');
     $session_cleanup_interval = isset($options['session_cleanup_interval']) ? $options['session_cleanup_interval'] : 'never';
-    ?>
+?>
     <select name="upt_settings[session_cleanup_interval]">
         <option value="never" <?php selected($session_cleanup_interval, 'never'); ?>>Never</option>
         <option value="weekly" <?php selected($session_cleanup_interval, 'weekly'); ?>>Weekly</option>
@@ -205,12 +215,13 @@ function upt_session_cleanup_interval_render() {
         <option value="yearly" <?php selected($session_cleanup_interval, 'yearly'); ?>>Yearly</option>
     </select>
     <p class="description">Choose how often to automatically delete session data to prevent database growth.</p>
-    <?php
+<?php
 }
 
 // Handle delete session data action
 add_action('admin_post_upt_delete_session_data', 'upt_handle_delete_session_data');
-function upt_handle_delete_session_data() {
+function upt_handle_delete_session_data()
+{
     if (check_admin_referer('upt_delete_session_data_action', 'upt_delete_session_data_nonce')) {
         if (!current_user_can('manage_options')) {
             wp_die('Unauthorized user');
@@ -229,7 +240,8 @@ function upt_handle_delete_session_data() {
 }
 
 // Function to delete all session data
-function upt_delete_all_session_data() {
+function upt_delete_all_session_data()
+{
     global $wpdb;
     $table_name = $wpdb->prefix . 'upt_sessions';
     $wpdb->query("TRUNCATE TABLE $table_name");
@@ -237,7 +249,8 @@ function upt_delete_all_session_data() {
 
 // Schedule automatic session data cleanup
 register_activation_hook(__FILE__, 'upt_schedule_cleanup');
-function upt_schedule_cleanup() {
+function upt_schedule_cleanup()
+{
     $options = get_option('upt_settings');
     $interval = isset($options['session_cleanup_interval']) ? $options['session_cleanup_interval'] : 'never';
 
@@ -250,13 +263,15 @@ function upt_schedule_cleanup() {
 
 // Hook into plugin deactivation to clear scheduled events
 register_deactivation_hook(__FILE__, 'upt_clear_scheduled_cleanup');
-function upt_clear_scheduled_cleanup() {
+function upt_clear_scheduled_cleanup()
+{
     wp_clear_scheduled_hook('upt_cleanup_event');
 }
 
 // Schedule the cleanup event
 add_action('upt_cleanup_event', 'upt_cleanup_session_data');
-function upt_cleanup_session_data() {
+function upt_cleanup_session_data()
+{
     global $wpdb;
     $table_name = $wpdb->prefix . 'upt_sessions';
     $wpdb->query("TRUNCATE TABLE $table_name");
@@ -264,7 +279,8 @@ function upt_cleanup_session_data() {
 
 // Update the scheduled event when settings are saved
 add_action('update_option_upt_settings', 'upt_update_scheduled_cleanup', 10, 2);
-function upt_update_scheduled_cleanup($old_value, $value) {
+function upt_update_scheduled_cleanup($old_value, $value)
+{
     upt_clear_scheduled_cleanup();
     $interval = isset($value['session_cleanup_interval']) ? $value['session_cleanup_interval'] : 'never';
 
@@ -275,7 +291,8 @@ function upt_update_scheduled_cleanup($old_value, $value) {
 
 // Add custom intervals for cron schedules
 add_filter('cron_schedules', 'upt_add_custom_cron_intervals');
-function upt_add_custom_cron_intervals($schedules) {
+function upt_add_custom_cron_intervals($schedules)
+{
     $schedules['weekly'] = array(
         'interval' => 604800, // 1 week in seconds
         'display'  => __('Once Weekly')
@@ -292,7 +309,8 @@ function upt_add_custom_cron_intervals($schedules) {
 }
 
 // Function to display the settings page
-function upt_options_page() {
+function upt_options_page()
+{
     // Display messages if any
     if (isset($_GET['upt_message'])) {
         if ($_GET['upt_message'] == 'deleted') {
@@ -301,7 +319,7 @@ function upt_options_page() {
             echo '<div class="error notice is-dismissible"><p>Security check failed. Please try again.</p></div>';
         }
     }
-    ?>
+?>
     <div class="wrap">
         <h1>URL Parameter Tracker Settings</h1>
         <form action='options.php' method='post'>
@@ -312,12 +330,13 @@ function upt_options_page() {
             ?>
         </form>
     </div>
-    <?php
+<?php
 }
 
 // Function to display the session data page
-function upt_session_data_page() {
-    ?>
+function upt_session_data_page()
+{
+?>
     <div class="wrap">
         <h1>User Session Data</h1>
         <p>This table displays the session data collected from users who visited your site with tracked URL parameters.</p>
@@ -375,12 +394,13 @@ function upt_session_data_page() {
             </tbody>
         </table>
     </div>
-    <?php
+<?php
 }
 
 // Create database table on plugin activation
 register_activation_hook(__FILE__, 'upt_create_sessions_table');
-function upt_create_sessions_table() {
+function upt_create_sessions_table()
+{
     global $wpdb;
     $table_name = $wpdb->prefix . 'upt_sessions';
 
@@ -401,7 +421,8 @@ function upt_create_sessions_table() {
 // Handle AJAX request to store session data
 add_action('wp_ajax_nopriv_upt_store_session_data', 'upt_store_session_data');
 add_action('wp_ajax_upt_store_session_data', 'upt_store_session_data');
-function upt_store_session_data() {
+function upt_store_session_data()
+{
     if (isset($_POST['sessionData']) && is_array($_POST['sessionData'])) {
         global $wpdb;
         $table_name = $wpdb->prefix . 'upt_sessions';
